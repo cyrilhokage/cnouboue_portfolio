461e37c60a2af5f76a9c2b1a53a6d88ceeb71942
cyril <cyrilhokage@yahoo.fr>
2020-11-12 07:18:23 +0100

    Change static files directory path

--- cnouboue_portfolio/settings.py
+++ cnouboue_portfolio/settings.py
@@ -150,16 +150,18 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 if os.environ.get('ENV') == 'PRODUCTION':
 
     #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
     # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    #PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
     # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-       os.path.join(PROJECT_ROOT, 'static'),
-    )
+    #STATICFILES_DIRS = (
+    #   os.path.join(PROJECT_ROOT, 'static'),
+    #)
 
 #Configure the media base dir and medias urls
 MEDIA_URL = '/media/'
