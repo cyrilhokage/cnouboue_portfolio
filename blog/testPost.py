73dc18e2702b671f176bac057deb940799f557c8
cyril <cyrilhokage@yahoo.fr>
2020-11-14 00:48:33 +0100

    Add test file

---
+++ blog/testPost.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from blog.models import Post
+
+
+class PostTestCase(TestCase):
+    
+    def testPost(self):
+
+        post = Post(title="My Title", technos="Blurb", summary="Post Body")
+        self.assertEqual(post.title, "My Title")
+        self.assertEqual(post.technos, "Blurb")
+        self.assertEqual(post.summary, "Post Body")
--- cnouboue_portfolio/settings.py
+++ cnouboue_portfolio/settings.py
@@ -170,3 +170,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
+
