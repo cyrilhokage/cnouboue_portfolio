da0bee16d9e1c4caf8d99407d2f930c4571943ca
cyrilhokage <cyrilhokage@outlook.fr>
2021-01-17 11:15:40 +0100

    Update posts publication and resume

--- blog/github_code.py
+++ blog/github_code.py
@@ -4,6 +4,17 @@ import os
 import base64
 import time, threading
 from slugify import slugify
+import requests
+
+def get_README(repo):
+  find_readme = False
+  for content in repo.get_contents(""):
+
+    if(content.path == "README.md"):
+      find_readme = True
+      return requests.get(content.download_url).text
+  
+  return find_readme
 
 def update_posts_from_github(request, username):
     # Github username
@@ -17,18 +28,22 @@ def update_posts_from_github(request, username):
     for repo in user.get_repos(username):
 
         # print(repo.name)
+        name_repo = repo.name.replace("_", " ").replace("-", " ").title()
         try:
-            post = Post.objects.get(title=repo.name)
-            post.updated_on=repo.pushed_at
-            post.github_link=repo.svn_url
+            post = Post.objects.get(title=name_repo)
+            post.updated_on = repo.pushed_at
+            post.github_link = repo.svn_url
+            post.summary = repo.description if repo.description is not None else "SUMMARY TO FILL"
+            post.content = get_README(repo)
         except Post.DoesNotExist : 
             post, created = Post.objects.update_or_create(
-                title=repo.name.replace("_", " ").replace("-", " ").title(),
+                title=name_repo,
                 slug = slugify(repo.name),
                 author=request.user,
                 updated_on=repo.pushed_at,
                 created_on=repo.created_at,
                 github_link=repo.svn_url,
+                content = get_README(repo),
                 technos=repo.language if repo.language is not None else "TECHNOS TO FILL",
                 summary=repo.description if repo.description is not None else "SUMMARY TO FILL",
             )
--- blog/models.py
+++ blog/models.py
@@ -19,6 +19,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
+    html_content = models.TextField( blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     main_image = models.ImageField(upload_to='images/', blank=True)
