795bc760c32113cb06054364c93e5e54284d712b
cyrilhokage <cyrilhokage@outlook.fr>
2021-04-10 01:20:58 +0200

    Delete user test case

--- blog/tests.py
+++ blog/tests.py
@@ -4,18 +4,3 @@ from notebook.models import Profile
 
 
 # Create your tests here.
-
-
-class UserTestCase(TestCase):
-    def test_user(self):
-        username = "shetu"
-        password = "hello"
-        u = User(username=username)
-        u.set_password(password)
-        p = Profile(
-            user=u,
-            pic="https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/MarioNSMBUDeluxe.png/220px-MarioNSMBUDeluxe.png",
-        )
-        u.save()
-        self.assertEqual(u.username, username)
-        self.assertTrue(u.check_password(password))
