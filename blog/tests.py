dca77fd21df8654b65a4a421bf3763f96c533835
cyrilhokage <cyrilhokage@outlook.fr>
2021-04-10 01:06:43 +0200

    Correct test error default pic

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from notebook.models import Profile
 
 
 # Create your tests here.
@@ -11,6 +12,10 @@ class UserTestCase(TestCase):
         password = "hello"
         u = User(username=username)
         u.set_password(password)
+        p = Profile(
+            user=u,
+            pic="https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/MarioNSMBUDeluxe.png/220px-MarioNSMBUDeluxe.png",
+        )
         u.save()
         self.assertEqual(u.username, username)
         self.assertTrue(u.check_password(password))
