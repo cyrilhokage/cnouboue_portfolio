1cf1716316a11af1da79034b72313d79fb60e4aa
cyrilhokage <cyrilhokage@outlook.fr>
2021-04-10 06:41:49 +0200

    Test Profile Creation

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,23 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from notebook.models import Profile
-
+from django.core.files import File
+import mock
 
 # Create your tests here.
+
+
+class UserTestCase(TestCase):
+    def test_user(self):
+        username = "shetu"
+        password = "hello"
+        file_mock = mock.MagicMock(spec=File)
+        file_mock.name = 'image.jpg'
+        u = User(username=username)
+        u.set_password(password)
+        # u.profile.pic = file_mock
+        p = Profile( pic=file_mock)
+        p.user = u
+        u.save()
+        self.assertEqual(u.username, username)
+        self.assertTrue(u.check_password(password))
--- notebook/tests.py
+++ notebook/tests.py
@@ -2,4 +2,26 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import Profile, Program, ViewProgram
 
+"""
 # Create your tests here.
+class ProfileTest(TestCase):
+
+    def create_user(self, username="test", password="hello"):
+        return User.objects.create(username=username, password=password)
+
+    def create_profile(self, slug="only-a-test", bio="yes, this is only a test"):
+        #user = self.create_user()
+        u =  User(username="test")
+        u.set_password("password")
+        u.save()
+        p = Profile.objects.create(user=u, slug=slug, bio=bio)
+        p.pic.url="https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/MarioNSMBUDeluxe.png/220px-MarioNSMBUDeluxe.png"
+        return p
+
+
+    def test_profile_creation(self):
+        p = self.create_profile()
+        #p.save()
+        self.assertTrue(isinstance(p, Profile))
+        self.assertEqual(w.__unicode__(), w.title)
+"""
