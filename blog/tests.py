70fbaf3895b331eb0102101ff565d10a9b554604
cyrilhokage <cyrilhokage@outlook.fr>
2021-04-10 06:54:53 +0200

    Fix test user issue

--- blog/tests.py
+++ blog/tests.py
@@ -11,13 +11,6 @@ class UserTestCase(TestCase):
     def test_user(self):
         username = "shetu"
         password = "hello"
-        file_mock = mock.MagicMock(spec=File)
-        file_mock.name = 'image.jpg'
-        u = User(username=username)
-        u.set_password(password)
-        # u.profile.pic = file_mock
-        p = Profile( pic=file_mock)
-        p.user = u
         u.save()
         self.assertEqual(u.username, username)
         self.assertTrue(u.check_password(password))
