f0f1bc59f2a7e16d73542f7265b02fa350dc13a3
cyrilhokage <cyrilhokage@outlook.fr>
2021-04-10 07:05:26 +0200

    Correct user tests

--- blog/tests.py
+++ blog/tests.py
@@ -11,6 +11,8 @@ class UserTestCase(TestCase):
     def test_user(self):
         username = "shetu"
         password = "hello"
+        u = User(username=username)
+        u.set_password(password)
         u.save()
         self.assertEqual(u.username, username)
         self.assertTrue(u.check_password(password))
