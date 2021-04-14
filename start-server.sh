e8c5ca05244815cd81ec976963516af36bfc6b89
cyrilhokage <cyrilhokage@outlook.fr>
2021-04-14 07:02:02 +0200

    Modify startting script

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # start-server.sh
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (python manage.py createsuperuser --no-input)
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('cyrilhokage', 'cyrilhokagen@yahoo.fr', 'pass')" | python manage.py shell
 fi
-(python manage.py collectstatic --no-input; gunicorn cnouboue_portfolio.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) & nginx -g "daemon off;"
+(python manage.py makemigrations ; python manage.py migrate; python manage.py collectstatic --no-input; gunicorn cnouboue_portfolio.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) & nginx -g "daemon off;"
